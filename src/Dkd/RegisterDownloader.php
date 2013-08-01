db698884bb44142c343cd19b76833b9ce3413eeb
Sascha Egerer <sascha.egerer@dkd.de>
2013-08-01 09:46:16 +0200

    Added registerAfterInstall command
    Command has to be called as "post-package-install" script
    to register the downloader after the package is installed

--- src/Dkd/RegisterDownloader.php
+++ src/Dkd/RegisterDownloader.php
@@ -4,15 +4,24 @@ namespace Dkd;
 
 use Composer\Script\Event;
 
-class RegisterDownloader {
+class RegisterDownloader
+{
 
-	public static function register(Event $event) {
-		$event->getComposer()->getDownloadManager()->setDownloader(
-			't3x',
-			new Downloader\T3xDownloader(
-				$event->getIO(),
-				$event->getComposer()->getConfig()
-			)
-		);
-	}
+    public static function register(Event $event)
+    {
+        $event->getComposer()->getDownloadManager()->setDownloader(
+            't3x',
+            new Downloader\T3xDownloader(
+                $event->getIO(),
+                $event->getComposer()->getConfig()
+            )
+        );
+    }
+
+    public static function registerAfterInstall(Event $event)
+    {
+        if ($event->getOperation()->getPackage()->getName() == 'dkd/downloaders') {
+            self::register($event);
+        }
+    }
 }
