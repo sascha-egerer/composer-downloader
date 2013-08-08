a0ebaf66724ce3eafc0169b3bfbf43d2ee34994e
Sascha Egerer <sascha.egerer@dkd.de>
2013-08-08 11:21:42 +0200

    Added Cache for the T3xDownloader so downloaded packages are cached now

--- src/Dkd/RegisterDownloader.php
+++ src/Dkd/RegisterDownloader.php
@@ -2,24 +2,39 @@
 
 namespace Dkd;
 
+use Composer\Cache;
 use Composer\Script\Event;
+use Composer\Script\PackageEvent;
 
 class RegisterDownloader
 {
-
+    /**
+     * Register dkd downloader
+     *
+     * @param Event $event
+     * @return void
+     */
     public static function register(Event $event)
     {
+        $config = $event->getComposer()->getConfig();
+        $io = $event->getIO();
+        $cache = null;
+        if ($config->get('cache-files-ttl') > 0) {
+            $cache = new Cache($io, $config->get('cache-files-dir'), 'a-z0-9_./');
+        }
         $event->getComposer()->getDownloadManager()->setDownloader(
             't3x',
-            new Downloader\T3xDownloader(
-                $event->getIO(),
-                $event->getComposer()->getConfig()
-            )
+            new Downloader\T3xDownloader($io, $config, $cache)
         );
     }
 
-    public static function registerAfterInstall(Event $event)
-    {
+    /**
+     * Register dkd downloader
+     *
+     * @param PackageEvent $event
+     * @return void
+     */
+    public static function registerAfterInstall(PackageEvent $event)    {
         if ($event->getOperation()->getPackage()->getName() == 'dkd/downloaders') {
             self::register($event);
         }
